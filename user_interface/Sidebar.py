850784aa532a742fb63957f664be0b1834c7728e
hafdaoui <lilpump0629@gmail.com>
2024-01-04 17:55:27 +0100

    17:55

---
+++ user_interface/Sidebar.py
@@ -0,0 +1,102 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO, BytesIO
+import json
+
+class Sidebar:
+    def __init__(self):
+        self.uploaded_file = None
+        self.dataframe = None
+        self.target_class = None
+        self.confirm_target_class = False
+
+    def process_file(self):
+        self.uploaded_file = st.sidebar.file_uploader("Choose a file")
+        if self.uploaded_file is not None:
+            if self.uploaded_file.name.endswith('.csv'):
+                # CSV file
+                self.dataframe = pd.read_csv(self.uploaded_file)
+                self.select_target_class()
+            elif self.uploaded_file.name.endswith('.json'):
+                # JSON file
+                string_data = self.uploaded_file.getvalue().decode("utf-8")
+                data = json.loads(string_data)
+                self.dataframe = pd.DataFrame(data)
+                self.select_target_class()
+            elif self.uploaded_file.name.endswith(('.xls', '.xlsx')):
+                # Excel file
+                bytes_data = self.uploaded_file.getvalue()
+                self.dataframe = pd.read_excel(BytesIO(bytes_data))
+                self.select_target_class()
+            st.write(self.dataframe)
+        return self.dataframe
+
+    def select_target_class(self):
+        st.sidebar.subheader('Select Target Class')
+        target_col_options = self.dataframe.columns.tolist()
+        target_col_options.insert(0, "None")
+
+        self.target_class = st.sidebar.selectbox(
+            "Choose the target class column:",
+            target_col_options
+        )
+
+        st.sidebar.markdown(
+            """
+            <style>
+            .stButton > button {
+                display: block;
+                margin: auto;
+                width: 45%;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True,
+        )
+
+        confirm_button = st.sidebar.button("Confirm")
+
+        if confirm_button:
+            if self.target_class == "None":
+                st.sidebar.warning("Please select a target class!")
+            else:
+                self.confirm_target_class = True
+
+    def get_target_class(self):
+        return self.target_class
+
+    def handle_missing_values(self):
+        st.sidebar.subheader('Handling Missing Values')
+        missing_values_option = st.sidebar.selectbox(
+            "Choose an option:",
+            ("Imputation",
+             "Deletion")
+        )
+        return missing_values_option
+
+    def feature_scaling(self):
+        st.sidebar.subheader('Feature Scaling')
+        feature_scaling_option = st.sidebar.selectbox(
+            "Choose an option:",
+            ("Normalization",
+             "Standardization")
+        )
+        return feature_scaling_option
+
+    def encode_categorical_variables(self):
+        st.sidebar.subheader('Encoding Categorical Variables')
+        categorical_encoding_option = st.sidebar.selectbox(
+            "Choose an option:",
+            ("One-Hot Encoding",
+             "Label Encoding")
+        )
+        return categorical_encoding_option
+
+    def handling_outliers(self):
+        st.sidebar.subheader('Handling Outliers')
+        handling_outliers = st.sidebar.selectbox(
+            "Choose an option:",
+            ("Z-score",
+             "IQR")
+        )
+        return handling_outliers
---
+++ user_interface/main_window.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from Sidebar import Sidebar
+
+def display_data_information(dataframe):
+    st.subheader("Data Information")
+    st.write("Number of Rows:", dataframe.shape[0])
+    st.write("Number of Columns:", dataframe.shape[1])
+    st.write("Column Names:", dataframe.columns.tolist())
+    st.write("Data Types:", dataframe.dtypes)
+
+def display_missing_values(dataframe):
+    st.subheader("Missing Values")
+    missing_values = dataframe.isnull().sum()
+    st.write(missing_values)
+
+def display_descriptive_statistics(dataframe):
+    st.subheader("Descriptive Statistics")
+    st.write(dataframe.describe())
+def main():
+    file_processor = Sidebar()
+    dataframe = file_processor.process_file()
+
+    if file_processor.confirm_target_class:
+        st.sidebar.subheader('Selected Target Class:')
+        st.sidebar.write(file_processor.target_class)
+        handle_missing_values = file_processor.handle_missing_values()
+        feature_scaling = file_processor.feature_scaling()
+        categorical_encoding_option = file_processor.encode_categorical_variables()
+        handling_outliers = file_processor.handling_outliers()
+
+        # Show data information
+        if dataframe is not None:
+            display_data_information(dataframe)
+            display_missing_values(dataframe)
+            display_descriptive_statistics(dataframe)
+
+if __name__ == "__main__":
+    main()
+
